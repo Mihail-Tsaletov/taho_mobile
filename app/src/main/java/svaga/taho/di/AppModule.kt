e6e2c825cdc6b0e4b97aaf79c0655497009b1d63
Mihail-Tsaletov <165092350+Mihail-Tsaletov@users.noreply.github.com>
2026-01-03 17:02:56 +0300

    работает подписка SSE

--- app/src/main/java/svaga/taho/di/AppModule.kt
+++ app/src/main/java/svaga/taho/di/AppModule.kt
@@ -59,7 +59,7 @@ object AppModule {
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
             .connectTimeout(15, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(0, TimeUnit.SECONDS)
             .addInterceptor(
                 HttpLoggingInterceptor { Log.d("OKHTTP", it) }.apply {
                     level = HttpLoggingInterceptor.Level.HEADERS
